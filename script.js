bfbf2319e1cfb7698c778d45d821fd2e56286fdf
sanjeev113f <sanjeevkumar113f@gmail.com>
2022-12-05 23:29:07 +0530

    Added Fetching result from Api

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ const result = document.getElementById("id1")
 form.addEventListener("submit", function(event) {
 	event.preventDefault();
 	const content = document.getElementById("content").value;
-	
+	fetch('http://localhost:8080/text/analyze', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				"content": content
+			})
+		})
+		.then(response => response.json())
+		.then(response => {
+			console.log(response)
+			result.innerHTML = response    // value
+		})
 });
