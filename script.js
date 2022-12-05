fffdf704e412cebd8b4ac1026ab90539421254d3
sanjeev113f <sanjeevkumar113f@gmail.com>
2022-12-05 23:25:10 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("myForm");
+const result = document.getElementById("id1")
+
+
+form.addEventListener("submit", function(event) {
+	event.preventDefault();
+	const content = document.getElementById("content").value;
+	
+});
